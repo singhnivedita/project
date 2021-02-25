9b4e088dfb8c914438ccf98d87c8c8862229b27f
Nivedita Singh <65302110+singhnivedita@users.noreply.github.com>
2021-02-25 10:24:18 +0530

    repo

--- Back End/dmcapp/src/main/java/com/project/dmcapp/service/PatientService.java
+++ Back End/dmcapp/src/main/java/com/project/dmcapp/service/PatientService.java
@@ -1,6 +1,24 @@
 package com.project.dmcapp.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.dmcapp.entities.DiagnosticService;
+import com.project.dmcapp.repo.DiagnosticServiceRepo;
+import com.project.dmcapp.repo.PatientRepo;
+
+@Service 
 public class PatientService {
 	
+	@Autowired
+	PatientRepo pateintrepo;
 	
+	@Autowired
+	DiagnosticServiceRepo diagnosisServiceRepo;
+	
+	public List<DiagnosticService> getDiagnosticService(){
+		return diagnosisServiceRepo.findAll();
+	}
 }
