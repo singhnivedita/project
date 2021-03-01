d2d4fa2f5f0b09eb53c0d1fb381a5d8bbb15fb6d
Manish Kumar <35579439+bsmanish028@users.noreply.github.com>
2021-03-02 01:19:33 +0530

    react file

---
+++ src/admin/AdminHome.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+
+
+// import axios from 'axios';
+import { Link, Redirect } from 'react-router-dom';
+
+
+class AdminHome extends React.Component{
+
+    
+	render(){
+       
+        
+			return(
+                
+                    <div className="wrapper d-flex " id="dashWrap">
+                    <div className="sidebar"> <small className="text-muted pl-4"></small>
+                        <ul className="list-bullet-no">
+                            <li><a href="#"><i className="fa fa-home"></i>Admin Dashboard</a></li>
+                            <li><a href="#"><i className="fa fa-credit-card"></i> Diagnostic Services </a></li>
+                        </ul> <small className="text-muted px-3">APPOINTMENT</small>
+                        <ul className="list-bullet-no">
+                            <li><a href="#"><i className="far fa-calendar-alt"></i>Book Appointment</a></li>
+                            <li><a href="#"><i className="fas fa-video"></i>Appointment Status</a></li>
+                        </ul> <small className="text-muted px-3">TEST REPORTS</small>
+                        <ul className="list-bullet-no">
+                            <li><a href="#"><i className="fa fa-external-link-alt"></i>Test Results</a></li>
+                            <li><a href="#"><i className="fa fa-help"></i>Treatment History</a></li>
+                         </ul>
+                         {/*<small className="text-muted px-3">OTHERS</small>
+                        
+                        <ul>
+                            <li><a href="#"><i className="fa fa-external-link-alt"></i>Help</a></li>
+                        </ul> */}
+                    </div>
+                </div>
+            
+        
+                    
+                );
+		
+			
+		}
+
+		
+		
+	
+}
+
+export default AdminHome;
---
+++ src/home/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class About extends React.Component{
+	render(){
+		return(
+			<div>
+			<h3> Welcome to the About Page of Student Management Portal </h3>
+			</div>
+
+			
+		);
+		
+	}
+}
+
+export default About;
